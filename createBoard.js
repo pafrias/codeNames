9cc5907b7f8982bba74e18a849825c98db058c1a
Pablo Frias <pafrias90@gmail.com>
2020-08-08 15:09:36 -0700

    Simplify piece placing logic, implement spec file

--- createBoard.js
+++ createBoard.js
@@ -3,55 +3,34 @@
 function createBoard() {
   let board = 0,
   p1Agents = 0,
-  p2Agents =  0,
+  p2Agents = 0,
   p1Sass =  0,
   p2Sass = 0;
 
   let rand = () => Math.floor(Math.random() * 25)
 
-  // insert player 1 agents
-  for (let i = 0; i < 8;) {
+  for (let i = 0; i < 22; i++) {
     let j = 2**rand();
-    if (board !== (board | j)) {
-      board |= j;
-      p1Agents |= j;
-      i++;
-    }
-  }
+    while (board === (board |= j)) j = 2**rand();
 
-  // insert player 1 assassins
-  for (let i = 0; i < 3;) {
-    let j = 2**rand();
-    if (board !== (board | j)) {
-      board |= j;
-      p1Sass |= j;
-      i++;
-    }
-  }
-
-  // insert player 2 agents
-  for (let i = 0; i < 8;) {
-    let j = 2**rand();
-    if (board !== (board | j)) {
-      board |= j;
-      p2Agents |= j;
-      i++;
-    }
-  }
-
-  // insert player 2 assassins
-  for (let i = 0; i < 3;) {
-    let j = 2**rand();
-    if (board !== (board | j)) {
-      board |= j;
-      p2Sass |= j;
-      i++;
-    }
+    if ([3,6,9].includes(i)) p1Sass |= j;
+    else if ([4,7,10].includes(i)) p2Sass |= j;
+    else if (i % 2) p1Agents |= j;
+    else p2Agents |= j;
   }
 
   return [p1Agents, p1Sass, p2Agents, p2Sass];
 }
 
+function createGameSet(N) {
+  let permutations = new Set();
+  while(permutations.size < N) {
+    permutations.add(createBoard());
+  }
+  return permutations;
+}
+
 module.exports = {
-  createBoard
+  createBoard,
+  createGameSet
 }
---
+++ createBoard.spec.js
@@ -0,0 +1,24 @@
+const { createBoard} = require('./createBoard');
+
+function testFunc(N) {
+  let err = false;
+  let perm = new Set();
+  for (let i = 0; i < N && N < 2300; i++) {
+    if (perm.size === perm.add(createBoard()).size) {
+      console.log(`Repeat at permutation #${i + 1}`);
+      err = true;
+    }
+  }
+  if (err) console.log(`${perm.size} of ${N} boards created`);
+  else console.log(`all ${N} boards unique!`);
+}
+
+function logSets(N) {
+  for (let i = 0; i < N; i++) {
+    console.log(createBoard());
+  }
+}
+
+logSets(20);
+//testFunc(2560);
+
