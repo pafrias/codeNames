a8011d2d82cd6894313fc24bc642a1019db44934
Pablo Frias <pafrias90@gmail.com>
2020-08-09 11:22:55 -0700

    Encapsulate Redis interaction in model.js

--- model.js
+++ model.js
@@ -1,5 +1,4 @@
 const Promise = require("bluebird");
-
 const redis = require('redis');
 const cache = redis.createClient();
 Promise.promisifyAll(cache);
@@ -17,8 +16,20 @@ cache.setAsync('foo', 'bar').then(reply => {
 }).catch(e => {
   console.log('error creating redis client\n', e);
   process.exit(0);
+  
 });
 
+async function getGameData (gameID, player) {
+  return cache.getAsync(gameID).then(reply => {
+    let data = eval(reply);
+    return {
+      agents: player === "1" ? data[0] : data[2],
+      sass: player === "1" ? data[1] : data[3]
+    }
+  });
+};
+
 module.exports = {
-  cache
+  cache,
+  getGameData
 }
